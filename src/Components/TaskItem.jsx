7be8bba78751c0681b8f635a3b078ca48914189c
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-04-06 21:23:56 +0000

    deuxieme commit

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -5,31 +5,28 @@ import "./style.css"
 
 const TaskItem = ({task, deleteTask, toggleComplete, handleEdit}) => {
     return(
-        <> <div className="taskItems">
-
-        <li type="none" className="taskItemCart"
-        style={{textDecoration: task.completed ? "line-through" : "none",}}
-        >
-          <div className="checkbox">
-          <input type="checkbox" checked={task.completed}
-          onClick={() => toggleComplete(task.id)}/>
-
-          <h6 className="tacheId">La tache {task.id}</h6>
+        <> 
+          <div className="taskItems">
+            <li type="none" className="taskItemCart"
+            style={{textDecoration: task.completed ? "line-through" : "none",}}
+            >
+                <div className="checkbox">
+                  <input type="checkbox" checked={task.completed}
+                  onClick={() => toggleComplete(task.id)}/>
+                  <h6 className="tacheId">La tache {task.id}</h6>
+                </div>
+                <div className="taskItem">
+                  <h4 onClick={() =>handleEdit(task)}>{task.name} </h4>
+                </div>
+                <div className="taskItem">
+                  <p>{task.description} </p>
+                </div>
+            </li>
+            <div className="taskBtn">
+                <button className="completeBtn" onClick={() => toggleComplete(task.id)}>{task.completed ? "Défaire" : "Completer"}</button>
+                <button className="deleteBtn" onClick={() => deleteTask(task.id)}>supprimer</button>
+            </div>
           </div>
-        <div className="taskItem">
-
-          <h4 onClick={() =>handleEdit(task)}>{task.name} </h4>
-
-          <p>{task.description} </p>
-        </div>
-        <div className="taskBtn">
-            
-            <button className="completeBtn" onClick={() => toggleComplete(task.id)}>{task.completed ? "Défaire" : "Completer"}</button>
-            <button className="deleteBtn" onClick={() => deleteTask(task.id)}>Delete</button>
-          
-        </div>
-        </li>
-        </div>
         </>
     )
 } 
